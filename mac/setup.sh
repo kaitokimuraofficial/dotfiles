cb628d4115c0362edfee487b0705458882b8a7fa
kaitokimuraofficial <k.kaito0422@akane.waseda.jp>
2025-02-04 18:10:43 +0900

    Add setup.sh

---
+++ mac/setup.sh
@@ -0,0 +1,49 @@
+#!/bin/zsh -eu
+
+# Install Homebrew and add it to the system PATH
+if ! type brew >/dev/null 2>&1; then
+  echo "----------------------------------------------------------------------------"
+  echo "installing brew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> /Users/ka.kimura/.zprofile
+  eval $(/opt/homebrew/bin/brew shellenv)
+fi
+
+
+echo "----------------------------------------------------------------------------"
+echo "Setting up config files..."
+ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
+
+echo "----------------------------------------------------------------------------"
+echo "please set up iTerm2"
+
+echo "----------------------------------------------------------------------------"
+echo "PLEASE SET UP FOR GIT ( ssh-key, name, email )"
+
+if ! type git >/dev/null 2>&1; then
+  echo "----------------------------------------------------------------------------"
+  echo "installing git..."
+  brew install git
+fi
+
+echo "> git config --global user.name \"USERNAME\""
+echo "> git config --global user.email \"EMAIL\""
+echo ""
+echo "ssh-keygen"
+echo "Enter file in which to save the key (/Users/USERNAME/.ssh/id_rsa):"
+echo "Enter passphrase (empty for no passphrase): "
+echo "pbcopy < ~/.ssh/HOGE.pub"
+echo "Add the SSH public key to GitHub"
+echo "ssh -T git@github.com"
+echo "If the connection is successful, you should see the following message:"
+echo "> Hi username! You've successfully authenticated, but GitHub does not provide shell access."
+echo ""
+echo "When trying to connect via HTTPS from the terminal, you will be prompted for your GitHub username and password."
+echo "Username for 'https://github.com': USERNAME"
+echo "Password for 'https://username@github.com': PAT"
+
+
+echo "----------------------------------------------------------------------------"
+echo "Success!"
+
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,23 +1,4 @@
-export NVM_DIR="$HOME/.nvm"
-export PATH="$HOME/.cargo/bin:$PATH"
-export PATH="/opt/homebrew/opt/openjdk@17/bin:$PATH"
-export PATH="/opt/homebrew/bin:$PATH"
-export PATH="/usr/local/opt/libpq/bin:$PATH"
-export PATH="/usr/local/opt/postgresql@13/bin:$PATH"
-export PATH="$RBENV_ROOT/bin:$PATH"
-export RBENV_ROOT="$HOME/.rbenv"
-eval "$(rbenv init -)"
+echo "BE ALL THAT YOU CAN BE!"
 
-# This loads nvm
-[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"
-# This loads nvm bash_completion
-[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"
+export PS1="kaitokimura %1~ %# "
 
-# opam configuration
-[[ ! -r "$HOME/.opam/opam-init/init.zsh" ]] || source "$HOME/.opam/opam-init/init.zsh"  > /dev/null 2> /dev/null
-
-# ghcup-env
-[ -f "$HOME.ghcup/env" ] && source "$HOME.ghcup/env"
-autoload -U +X bashcompinit && bashcompinit
-complete -o nospace -C /opt/homebrew/bin/terraform terraform
-autoload -Uz compinit
