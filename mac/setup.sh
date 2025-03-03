18e2549615b6660a0e62fbb277c2fce63f461a20
kaitokimuraofficial <k.kaito0422@akane.waseda.jp>
2025-03-03 17:11:25 +0900

    Add coc-settings.json

--- mac/setup.sh
+++ mac/setup.sh
@@ -36,9 +36,10 @@ echo "--------------------------------------------------------------------------
 echo "Setting up config files..."
 ln -sf ~/dotfiles/zsh/zshenv ~/.zshenv
 ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
-
 ln -sf ~/dotfiles/zsh/zprofile ~/.zprofile
+
 ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
+ln -sf ~/dotfiles/vim/coc-settings.json ~/.config/vim/coc-settings.json
 
 echo "----------------------------------------------------------------------------"
 echo "please set up iTerm2"
