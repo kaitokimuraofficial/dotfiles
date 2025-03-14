54e16806500ea4e26707891fba68d0c4a470e0db
kaitokimuraofficial <k.kaito0422@akane.waseda.jp>
2025-03-15 01:50:53 +0900

    Fix

--- mac/setup.sh
+++ mac/setup.sh
@@ -1,64 +1,156 @@
-#!/bin/zsh -eu
+#!/bin/bash -eu
 
-# Install Homebrew and add it to the system PATH
-if ! type brew >/dev/null 2>&1; then
-  echo "----------------------------------------------------------------------------"
-  echo "installing brew..."
+readonly DOTFILES_DIR="${HOME}/dotfiles"
+readonly NVM_VER="0.39.7"
+readonly BREW_LIBS=(
+  "aws-vault --cask"
+  "git"
+  "tfenv"
+)
+
+function separator_line() {
+    echo "============================================================================"
+}
+
+function is_installed() {
+  type "$1" >/dev/null 2>&1
+}
+
+
+echo ""
+separator_line
+echo ""
+
+#######################################
+# Create symlink.
+#######################################
+symlink_src=(
+  "${HOME}/.vimrc"
+  "${HOME}/.vim/coc-settings.json"
+  "${HOME}/.zshenv"
+  "${HOME}/.zshrc"
+  "${HOME}/.zprofile"
+)
+
+symlink_dest=(
+  "${DOTFILES_DIR}/vim/vimrc"
+  "${DOTFILES_DIR}/vim/coc-settings.json"
+  "${DOTFILES_DIR}/zsh/zshenv"
+  "${DOTFILES_DIR}/zsh/zshrc"
+  "${DOTFILES_DIR}/zsh/zprofile"
+)
+
+echo "Set up config files..."
+for ((i=0; i<${#symlink_src[@]}; i++)); do
+  src=${symlink_src[$i]}
+  dest=${symlink_dest[$i]}
+
+  if [[ -L $src ]] && [[ $(readlink $src) == $dest ]]; then
+    echo "Skipping: ${src} already links to ${dest}."
+  else
+    echo "Creating symlink: ${src} -> ${dest}..."
+    ln -sf $dest $src
+  fi
+done
+
+echo ""
+separator_line
+echo ""
+
+
+#######################################
+# Install nvm first, then install node.js with it.
+#######################################
+echo "Install nvm and node..."
+if [ -s "$NVM_DIR/nvm.sh" ]; then
+  echo "Skipping: nvm is already installed."
+else
+  echo "Installing nvm..."
+  /bin/bash -c "$(curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v${NVM_VER}/install.sh)"
+fi
+
+source "$NVM_DIR/nvm.sh"
+
+nvm install --default --lts
+nvm use --lts
+
+echo ""
+separator_line
+echo ""
+
+
+#######################################
+# Install Homebrew and add it to the system PATH.
+#######################################
+echo "Install Homebrew..."
+if is_installed brew; then
+  echo "Skipping: Homebrew is already installed."
+else
+  echo "Installing Homebrew..."
+
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> /Users/ka.kimura/.zprofile
+  echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> /Users/${USER}/.zprofile
   eval $(/opt/homebrew/bin/brew shellenv)
 fi
 
-echo "Install node through nvm......"
-if ! command -v nvm &> /dev/null; then
-    echo "nvm not found. Install..."
-    /bin/bash -c "$(curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh)"
-    nvm install 22
-    nvm use 22
-    echo "nvm and Node.js installed successfully!"
-fi
-nvm version
-node -v
-
-
-echo "----------------------------------------------------------------------------"
-echo "Setting up config files..."
-ln -sf ~/dotfiles/zsh/zshenv ~/.zshenv
-ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
-ln -sf ~/dotfiles/zsh/zprofile ~/.zprofile
-
-ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
-ln -sf ~/dotfiles/vim/coc-settings.json ~/.vim/coc-settings.json
-
-echo "----------------------------------------------------------------------------"
-echo "please set up iTerm2"
-
-echo "----------------------------------------------------------------------------"
-echo "PLEASE SET UP FOR GIT ( ssh-key, name, email )"
-
-if ! type git >/dev/null 2>&1; then
-  echo "----------------------------------------------------------------------------"
-  echo "installing git..."
-  brew install git
-fi
-
-echo "> git config --global user.name \"USERNAME\""
-echo "> git config --global user.email \"EMAIL\""
 echo ""
-echo "ssh-keygen"
-echo "Enter file in which to save the key (/Users/USERNAME/.ssh/id_rsa):"
-echo "Enter passphrase (empty for no passphrase): "
-echo "pbcopy < ~/.ssh/HOGE.pub"
-echo "Add the SSH public key to GitHub"
-echo "ssh -T git@github.com"
-echo "If the connection is successful, you should see the following message:"
-echo "> Hi username! You've successfully authenticated, but GitHub does not provide shell access."
+separator_line
 echo ""
-echo "When trying to connect via HTTPS from the terminal, you will be prompted for your GitHub username and password."
-echo "Username for 'https://github.com': USERNAME"
-echo "Password for 'https://username@github.com': PAT"
 
 
-echo "----------------------------------------------------------------------------"
-echo "Success!"
+#######################################
+# Install libraries through Homebrew.
+#######################################
+echo "Install libraries through Homebrew..."
+for arg in "${BREW_LIBS[@]}"; do
+  if [[ $arg =~ \  ]]; then
+    LIB=$(echo $arg | cut -d' ' -f1)
+    OPTS=$(echo $arg | cut -d' ' -f2-)
+
+    if is_installed $LIB; then
+      echo "Skipping: ${LIB} already installed."
+    else
+      echo "Installing ${LIB} with ${OPTS}..."
+      brew install $LIB $OPTS
+    fi
+  else
+    if is_installed $arg; then
+      echo "Skipping: ${arg} already installed."
+    else
+      echo "Installing ${arg}..."
+      brew install $arg
+    fi
+  fi
+done
+
+echo ""
+separator_line
+echo ""
+
+
+#######################################
+# Set up git.
+#######################################
+if ! git config --global user.name>/dev/null 2>&1; then
+  echo "============= DO IT =============="
+  echo "Your name is not set in git config"
+  echo ""
+  echo "> git config --global user.name \"USERNAME\""
+  echo "=================================="
+fi
+
+if ! git config --global user.email>/dev/null 2>&1; then
+  echo "============= DO IT =============="
+  echo "Your email is not set in git config"
+  echo ""
+  echo "> git config --global user.name \"EMAIL\""
+  echo "=================================="
+fi
+
+if ssh -T git@github.com>/dev/null 2>&1; then
+  echo "============= DO IT =============="
+  echo "Your SSH key is either not added to GitHub or not being used correctly."
+  echo ""
+  echo "=================================="
+fi
 
