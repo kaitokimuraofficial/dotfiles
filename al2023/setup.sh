7362e675ff013473ce4a939c67352eca7fc52cb4
Kai <112356027+kaitokimuraofficial@users.noreply.github.com>
2025-07-05 09:47:39 +0900

    Update setup.sh

--- al2023/setup.sh
+++ al2023/setup.sh
@@ -77,7 +77,7 @@ else
   echo "Installing Homebrew..."
 
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/kaitokimura/.zshrc
+  echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> "${HOME}/.zshrc"
   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
 
